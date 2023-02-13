4c15be0892795802f022ab007449da7823c9bab7
Taylor Nurmi <tnurmi0714@gmail.com>
2023-02-13 11:37:23 -0700

    PUSH

--- app.js
+++ app.js
@@ -7,24 +7,34 @@ const background = (
 );
 const title = "";
 let images = [];
-for (let animal in  animals ) {
+for (let animal in animals) {
     images.push(
     <img
-        key={ animal }
-        className= "animal" 
-        alt={ animal }
-        src={ animals[animal].image }
-        aria-label={ animal }
+        key={animal}
+        className="animal"
+        alt={animal}
+        src={animals[animal].image}
+        aria-label={animal}
         role="button"
+        onClick={displayFact}
     />
     );
 }
 const animalFacts = (
     <div>
         {background}
+        <p id="fact"></p>
         <div className="animals">{images}</div>
         <h1>{title === "" ? "Click an animal for a fun fact" : title}</h1>
     </div>
 );
+function displayFact(e) {
+    let selectedAnimal = e.target.alt;
+    let optionIndex = Math.floor(
+    Math.random() * animals[selectedAnimal].facts.length
+    );
+    let funFact = animals[selectedAnimal].facts[optionIndex];
+    document.getElementById("fact").innerHTML = funFact;
+}
 
 ReactDOM.render(animalFacts, document.getElementById("root"));
