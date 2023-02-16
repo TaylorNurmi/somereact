e7bbfc8e6345387b2075d7107d2f11f034dda011
Taylor Nurmi <tnurmi0714@gmail.com>
2023-02-15 21:11:16 -0700

    PUSH

--- simple clock/clock.js
+++ simple clock/clock.js
@@ -6,15 +6,36 @@ export class Clock extends React.Component {
         this.state = { date: new Date() };
     }
     render() {
-        return <div>{this.state.date.toLocaleTimeString()}</div>;
+        return (
+        <div>
+            {this.props.isPrecise
+            ? this.state.date.toISOString()
+            : this.state.date.toLocaleTimeString()}
+        </div>
+        );
     }
-    componentDidMount() {
-        const oneSecond = 1000;
+    startInterval() {
+        let delay;
+        if (this.props.isPrecise) {
+        delay = 100;
+        } else {
+        delay = 1000;
+        }
         this.intervalID = setInterval(() => {
         this.setState({ date: new Date() });
-        }, oneSecond);
+        }, delay);
+    }
+    componentDidMount() {
+        this.startInterval();
+    }
+    componentDidUpdate(prevProps) {
+        if (this.props.isPrecise === prevProps.isPrecise) {
+        return;
+        }
+        clearInterval(this.intervalID);
+        this.startInterval();
     }
     componentWillUnmount() {
-        clearInterval(this.intervalID)
+        clearInterval(this.intervalID);
     }
 }
